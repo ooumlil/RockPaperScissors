eda94ebcfa736ed089f96b0157286f05c261d7c3
Oussama Oumlil <hamza.oumlil.5@gmail.com>
2023-06-26 18:06:44 +0100

    the game is working fine, to style it more and make it stop after one in the team reaches a certain won rounds

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const ROCK = "rock",
   SCISSORS = "scissors",
   choices = [ROCK, PAPER, SCISSORS];
 
+let computerCount = 0,
+  playerCount = 0;
+
+let rockButton = document.querySelector(`#rock`),
+  paperButton = document.querySelector(`#paper`),
+  scissorsButton = document.querySelector(`#scissors`),
+  playerScore = document.querySelector(`#player-score`),
+  result = document.querySelector(`#result`),
+  computerScore = document.querySelector(`#computer-score`);
+
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * 100) % 3];
 }
@@ -21,3 +31,66 @@ function playRound(playerSelection, computerSelection) {
     ? WINNER
     : LOSER;
 }
+
+rockButton.addEventListener(`click`, () => {
+  playerSelection = `rock`;
+  computerSelection = getComputerChoice();
+  let gameResult = playRound(playerSelection, computerSelection);
+  let WINNER = `You win! ${playerSelection} beats ${computerSelection}.`,
+    LOSER = `You lose! ${computerSelection} beats ${playerSelection}.`,
+    TIE = `Its a tie!`;
+  if (gameResult === WINNER) {
+    console.log(gameResult);
+    playerCount++;
+  } else if (gameResult === LOSER) {
+    console.log(gameResult);
+    computerCount++;
+  } else {
+    console.log(gameResult);
+  }
+  playerScore.textContent = `${playerCount}`;
+  computerScore.textContent = `${computerCount}`;
+  result.textContent = `${gameResult}`;
+});
+
+paperButton.addEventListener(`click`, () => {
+  playerSelection = `paper`;
+  computerSelection = getComputerChoice();
+  let gameResult = playRound(playerSelection, computerSelection);
+  let WINNER = `You win! ${playerSelection} beats ${computerSelection}.`,
+    LOSER = `You lose! ${computerSelection} beats ${playerSelection}.`,
+    TIE = `Its a tie!`;
+  if (gameResult === WINNER) {
+    console.log(gameResult);
+    playerCount++;
+  } else if (gameResult === LOSER) {
+    console.log(gameResult);
+    computerCount++;
+  } else {
+    console.log(gameResult);
+  }
+  playerScore.textContent = `${playerCount}`;
+  computerScore.textContent = `${computerCount}`;
+  result.textContent = `${gameResult}`;
+});
+
+scissorsButton.addEventListener(`click`, () => {
+  playerSelection = `scissors`;
+  computerSelection = getComputerChoice();
+  let gameResult = playRound(playerSelection, computerSelection);
+  let WINNER = `You win! ${playerSelection} beats ${computerSelection}.`,
+    LOSER = `You lose! ${computerSelection} beats ${playerSelection}.`,
+    TIE = `Its a tie!`;
+  if (gameResult === WINNER) {
+    console.log(gameResult);
+    playerCount++;
+  } else if (gameResult === LOSER) {
+    console.log(gameResult);
+    computerCount++;
+  } else {
+    console.log(gameResult);
+  }
+  playerScore.textContent = `${playerCount}`;
+  computerScore.textContent = `${computerCount}`;
+  result.textContent = `${gameResult}`;
+});
